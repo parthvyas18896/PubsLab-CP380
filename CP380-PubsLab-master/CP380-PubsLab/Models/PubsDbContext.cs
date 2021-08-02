ea60b5e73f679a4b8984e3c8e381b04d937cbb08
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-08-02 00:25:21 -0400

    Completed Database tasks

--- CP380-PubsLab-master/CP380-PubsLab/Models/PubsDbContext.cs
+++ CP380-PubsLab-master/CP380-PubsLab/Models/PubsDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using System.IO;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 
 namespace CP380_PubsLab.Models
@@ -15,37 +18,102 @@ namespace CP380_PubsLab.Models
 
         // TODO: Add DbSets
 
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Jobs> Jobs { get; set; }
+
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<Titles> Titles { get; set; }
+        public DbSet<Stores> Stores { get; set; }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // TODO
+            modelBuilder.Entity<Sales>().HasKey(a => new { a.stor_id, a.title_id });
+
+            modelBuilder
+                .Entity<Stores>()
+                .HasMany(a => a.Titles)
+                .WithMany(a => a.Stores)
+                .UsingEntity<Sales>(
+                  J => J
+                         .HasOne(at => at.Title)
+                         .WithMany(t => t.Sales)
+                         .HasForeignKey(at => at.title_id),
+                  J => J
+                         .HasOne(at => at.Store)
+                         .WithMany(t => t.Sales)
+                         .HasForeignKey(at => at.stor_id)
+
+                  );
+
         }
+
     }
 
 
     public class Titles
     {
         // TODO
+        [Key]
+        public string title_id { get; set; }
+        public string title { get; set; }
+        public List<Sales> Sales { get; set; }
+
+        public ICollection<Stores> Stores { get; set; }
+
     }
 
 
     public class Stores
     {
-        // TODO
+        // TODo
+        [Key]
+        public string stor_id { get; set; }
+        public string stor_name { get; set; }
+        public List<Sales> Sales { get; set; }
+        public ICollection<Titles> Titles { get; set; }
     }
-
-
     public class Sales
     {
         // TODO
+        [Column("stor_id")]
+        public string stor_id { get; set; }
+        [ForeignKey("stor_id")]
+        public Stores Store { get; set; }
+        [Column("title_id")]
+        public string title_id { get; set; }
+        [ForeignKey("title_id")]
+        public Titles Title { get; set; }
     }
 
     public class Employee
     {
         // TODO
+        [Key]
+        [Column("emp_id")]
+        public string Emp_id { get; set; }
+        [Column("fname")]
+        public string Firstname { get; set; }
+        [Column("lname")]
+        public string Lastname { get; set; }
+
+        [Column("job_id")]
+        public Int16 J_id { get; set; }
+        [ForeignKey("J_id")]
+        //[Column("job_id")]
+        public Jobs Job { get; set; }
+
     }
 
     public class Jobs
     {
         // TODO
+
+        [Key, Column("job_id")]
+        public Int16 J_id { get; set; }
+        [Column("job_desc")]
+        public string Desc { get; set; }
+        public List<Employee> Employee { get; set; }
     }
 }
--- CP380-PubsLab-master/CP380-PubsLab/Program.cs
+++ CP380-PubsLab-master/CP380-PubsLab/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using CP380_PubsLab.Models;
 
 namespace CP380_PubsLab
 {
@@ -6,36 +11,94 @@ namespace CP380_PubsLab
     {
         static void Main(string[] args)
         {
-            using (var dbcontext = new Models.PubsDbContext())
+            using var dbcontext = new Models.PubsDbContext();
+
+            if (dbcontext.Database.CanConnect())
             {
-                if (dbcontext.Database.CanConnect())
-                {
-                    Console.WriteLine("Yes, I can connect");
-                }
-
-                // 1:Many practice
-                //
-                // TODO: - Loop through each employee
-                //       - For each employee, list their job description (job_desc, in the jobs table)
-
-                // TODO: - Loop through all of the jobs
-                //       - For each job, list the employees (first name, last name) that have that job
-
-
-                // Many:many practice
-                //
-                // TODO: - Loop through each Store
-                //       - For each store, list all the titles sold at that store
-                //
-                // e.g.
-                //  Bookbeat -> The Gourmet Microwave, The Busy Executive's Database Guide, Cooking with Computers: Surreptitious Balance Sheets, But Is It User Friendly?
-                
-                // TODO: - Loop through each Title
-                //       - For each title, list all the stores it was sold at
-                //
-                // e.g.
-                //  The Gourmet Microwave -> Doc-U-Mat: Quality Laundry and Books, Bookbeat
+                Console.WriteLine("Yes, I can connect");
             }
+
+
+            //foreach (var j in dbcontext.Jobs)
+            //{
+            //  Console.WriteLine($"{j.desc}");
+            //}
+
+            // 1:Many practice
+            //
+            // TODO: - Loop through each employee
+            //       - For each employee, list their job description (job_desc, in the jobs table)
+
+
+            var q = dbcontext.Employee;
+            Console.WriteLine($"{q.ToQueryString()}");
+            foreach (var e in q.Include(e => e.Job))
+            {
+                Console.WriteLine($"{e.Emp_id} -> {e.Job.Desc}");
+            }
+
+
+
+            // TODO: - Loop through all of the jobs
+            //       - For each job, list the employees (first name, last name) that have that job
+
+
+            foreach (var a in dbcontext.Jobs.Include(d => d.Employee))
+            {
+
+
+                var employessjob = a.Employee.Select(d => new { EmpId = d.Emp_id, FName = d.Firstname, LName = d.Lastname, Job_Desc = a.Desc }).ToList();
+                var empl_Job = String.Join(",", employessjob);
+                Console.WriteLine($"{a.Desc} -> {empl_Job}");
+            }
+
+            //foreach (var a in dbcontext.Employee.Include(d => d.Job))
+            //{
+
+
+            //    var employessjob = a.Job.Select(a => new { Job_Id = a.job_id, Job_Desc = a.job_desc, Employee = a.Employee.Select(d => new { FirstName = d.fname, LastName = d.lname }) }).ToList();
+            //    var empl_Job = String.Join(",", employessjob);
+            //    Console.WriteLine($"{a.Firstname}{a.Lastname} -> {empl_Job}");
+            //}
+
+
+
+            // Many:many practice
+            //
+            // TODO: - Loop through each Store
+            //       - For each store, list all the titles sold at that store
+            //
+            //var q = dbcontext.Stores.Include(a => a.Sales);
+            //Console.WriteLine(q.ToQueryString());
+
+            var storeTitles = dbcontext.Stores.Select(s => new { Store = s.stor_name, Titles = s.Sales.Select(sl => sl.Title.title) }).ToList();
+
+
+            Console.WriteLine("\n#Stores | Titles ");
+            foreach (var store in storeTitles)
+            {
+                var strJoin = String.Join(",", store.Titles);
+                Console.WriteLine($"\n{store.Store} -> {strJoin}");
+            }
+            // e.g.
+            //  Bookbeat -> The Gourmet Microwave, The Busy Executive's Database Guide, Cooking with Computers: Surreptitious Balance Sheets, But Is It User Friendly?
+
+            // TODO: - Loop through each Title
+            //       - For each title, list all the stores it was sold at
+
+            var Titlestore = dbcontext.Titles.Select(t => new { TitleName = t.title, StoreList = t.Sales.Select(sl => sl.Store.stor_name) }).ToList();
+
+
+            Console.WriteLine("\n#Titles | Stores ");
+            foreach (var title in Titlestore)
+            {
+                var strJoin = String.Join(",", title.StoreList);
+                Console.WriteLine($"\n{title.TitleName} -> {strJoin}");
+            }
+            //
+            // e.g.
+            //  The Gourmet Microwave -> Doc-U-Mat: Quality Laundry and Books, Bookbeat
         }
     }
 }
+
